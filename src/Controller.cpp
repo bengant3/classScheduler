8449aa4624b4ccb632895f0899119ceaa039ab7e
bengant3 <bengant3@gmail.com>
2021-06-29 18:38:55 -0400

    small fix + more verbose feedback options

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -4,6 +4,9 @@
 
 #include "Controller.h"
 
+static std::string timeslotStrings[] = {"null", "unscheduled", "MWF9", "MWF10", "MWF11", "MWF12", "MWF1", "MWF2", "MWF3",
+                                     "MW4", "TH930", "TH11", "TH1", "TH230", "TH4"};
+
 Controller::Controller() {
     courseCatalog = Directory<Section>::instance();
     studentList = Directory<Student>::instance();
@@ -20,26 +23,30 @@ void Controller::resetDirectories() {
             Student* stu = sec.getRoster().back();
             sec.removeStudent(stu->getID());
         }
+        sec.scheduleAt(unscheduled);
     }
 }
 
 double Controller::runScheduler(bool verbose, int start) {
-    std::deque<int> newDeque;
-    double accuracy = -0.01;
-
-    newDeque = studentList->asIntDeque(start);
-    accuracy = scheduler.schedule(newDeque);
+    std::deque<int> newDeque = studentList->asIntDeque(start);
+    double accuracy = scheduler.schedule(newDeque);
 
     if(verbose) {
         std::cout << "Completed scheduling with a " << accuracy * 100 << "% success rate.\n\n";
-        std::cout << "See result?\n1. Yes, by student\n2. Yes, by class\n3. No\n";
+        std::cout << "See result?\n"
+                    << "1. Yes, by student\n"
+                    << "2. Yes, by student, first 10\n"
+                    << "3. Yes, by class\n"
+                    << "4. Yes, by class, first 10\n"
+                    << "5. No\n";
         char resp;
         std::cin >> resp;
 
         std::string result = "";
 
-        if (resp == '1') {
-            for (auto s = studentList->begin(); s != studentList->end(); ++s) {
+        if (resp == '1' || resp == '2') {
+            int iter = 0;
+            for (auto s = studentList->begin(); s != studentList->end() && (resp == '2' && iter < 10); ++s, ++iter) {
                 result += s->getName() + (s->getName().size() < 14 ? ": \t\t" : ": \t");
                 for (Section *const sec : s->getEnrolled()) {
                     result += sec->shortName();
@@ -47,9 +54,10 @@ double Controller::runScheduler(bool verbose, int start) {
                 }
                 result += "\n";
             }
-        } else if (resp == '2') {
-            for (auto s = courseCatalog->begin(); s != courseCatalog->end(); ++s) {
-                result += s->shortName() + ": \t";
+        } else if (resp == '3' || resp == '4') {
+            int iter = 0;
+            for (auto s = courseCatalog->begin(); s != courseCatalog->end() && (resp == '4' && iter < 10); ++s, ++iter) {
+                result += s->shortName() + " (" + timeslotStrings[s->getTime()] + ") : \t";
                 for (Student *const stu : s->getRoster()) {
                     result += stu->getName();
                     if (stu != *(--(s->getRoster().end()))) result += ", ";
